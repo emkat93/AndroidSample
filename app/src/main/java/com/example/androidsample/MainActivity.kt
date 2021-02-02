488209ad3dc090802a3544d22b5ab7a3ffc598c0
emkat93 <eallen5064@students.mchenry.edu>
2021-02-02 14:37:51 -0600

    Updated Code

--- app/src/main/java/com/example/androidsample/MainActivity.kt
+++ app/src/main/java/com/example/androidsample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.androidsample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,11 +11,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-    fun convertCurrency(view: View) = if (dollarText.text.isNotEmpty() {
-        val dollarValue = dollarText.text.toString().toFloat()
-        val euroValue = dollarValue * 0.85f
-        textView.text = euroValue.toString()
-    } else {
-        textView.text = getString(R.string.no_value_string)
+    fun convertCurrency(view: View) {
+        if (dollarText.text.isNotEmpty()) {
+                    val dollarValue = dollarText.text.toString().toFloat()
+                    val euroValue = dollarValue * 0.85f
+                    tv1.text = euroValue.toString()
+                } else {
+            tv1.text = getString(R.string.no_value_string)
+        }
     }
 }
